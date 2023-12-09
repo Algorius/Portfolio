07b7fbe0b95da542b323d24f98a784f424745b91
Algorius <150431845+Algorius@users.noreply.github.com>
2023-12-09 13:42:52 -0800

    Add files via upload
    These are after modification.

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+
+
+
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+    const controls = document.querySelectorAll(".control");
+
+    controls.forEach((button) => {
+        button.addEventListener("click", function () {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
-        })
+        });
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
+
+    const themeBtn = document.querySelector(".theme-btn");
+    const iconSun = '<i class="fas fa-sun" style="color: #ffd700;"></i>';
+    const iconMoon = '<i class="fas fa-moon" style="color: #000;"></i>';
+
+    themeBtn.addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+        updateThemeIcon();
+    });
+
+    function updateThemeIcon() {
+        const isLightMode = document.body.classList.contains("light-mode");
+        themeBtn.innerHTML = isLightMode ? iconSun : iconMoon;
+    }
+
+    // Initial theme icon setup
+    updateThemeIcon();
 })();
+
